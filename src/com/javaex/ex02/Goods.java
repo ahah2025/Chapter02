aded4cd9280222ab5627928fb3165f1fb97b82c6
ahah2025 <adghty61>
2025-04-29 11:01:42 +0900

    2025-04-29 객체지향프로그래밍 메소드

---
+++ src/com/javaex/ex02/Goods.java
@@ -0,0 +1,26 @@
+package com.javaex.ex02;
+
+//class(틀)
+public class Goods {
+
+	//필드
+	private String name;
+	public int price;
+	
+	//생성자
+	
+	//메소드
+	//name 값 입력
+	public void setName(String n) {
+		//n이 "니콘"
+		//위에 있는 name 입력
+		name = n;
+		//return 필요없는 경우 
+	}	
+	
+		//name 값 읽어오기
+	public String getName() {
+		return name;
+	}
+
+}
