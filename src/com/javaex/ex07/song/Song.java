fd5fbc00dd28871567b3f970d5b1b7894e84c9ab
ahah2025 <adghty61>
2025-04-29 17:34:06 +0900

    2025-04-29

--- src/com/javaex/ex07/song/Song.java
+++ src/com/javaex/ex07/song/Song.java
@@ -5,65 +5,65 @@ public class Song {
 	//필드
 	private String title; 	//노래의 제목
 	private String artist; 	//가수
-	private char album; 	//노래가 속한 앨범 제목
-	private char composer; 	//노래의 작곡가 
-	private char year;		//노래가 발표된 연도 나타냄
-	private char track;  	//노래가 속한 앨범에서 트랙 번호를 나타냄
+	private String album; 	//노래가 속한 앨범 제목
+	private String composer; 	//노래의 작곡가 
+	private String year;		//노래가 발표된 연도 나타냄
+	private String track;  	//노래가 속한 앨범에서 트랙 번호를 나타냄
 	
 	//생성자
 	
 	//메소드(gs)
 	// title 값 대입
-	public void settitle(String title) {
+	public void setTitle(String title) {
 		this.title = title;
 	}
 	//title 읽기
-	public String gettitle() {
+	public String getTitle() {
 		return title;
 	}
 	 
 	// artist 값 대입
-	public void setartist(String artist) {
+	public void setArtist(String artist) {
 		this.artist = artist;
 	}
 	//artist 읽기
-	public String getartist() {
+	public String getArtist() {
 		return artist;
 	}
 	
 	//album 값 대입
-	public void setalbum(char album) {
+	public void setAlbum(String album) {
 		this.album = album;
 	}
 	//album 읽기
-	public char getalbum() {
+	public String getAlbum() {
 		return album;
 	}
 	
 	//composer 값 대입
-	public void setcomposer(char composer) {
+	public void setComposer(String composer) {
 		this.composer = composer;
 	}
 	//composer 읽기
-	public char getcomposer() {
+	public String getComposer() {
 		return composer;
 	}
 	
 	//year 값 대입
-	public void setyear(char year) {
+	public void setYear(String year) {
 		this.year = year;
 	}
 	//year 읽기
-	public char getyear() {
+	public String getYear() {
 		return year;
 	}	
 	
 	//track 값 대입
-	public void settrack(char track) {
+	public void setTrack(String track) {
 		this.track = track;
 	}
 	//track 읽기
-	public char gettrack() {
+	public String getTrack() {
 		return track;
 	}
 	
@@ -71,8 +71,7 @@ public class Song {
 	
 	//노래정보 출력
 	public void showInfo(){
-		//System.out.println("artist1,title" + (album, year, track, composer);  
-		System.out.println("artist2,title" + year +title +", album" +   "]composer + 작곡");  
-		System.out.println("artist3,title" + year +title +", album" +   "]composer + 작곡");  
+		System.out.println(artist + ","+ title +","+"(" + album + "," + year + "," + track + "," + composer+")");  
+		
 	}
 }
--- src/com/javaex/ex07/song/SongApp.java
+++ src/com/javaex/ex07/song/SongApp.java
@@ -4,31 +4,31 @@ public class SongApp {
 
 	public static void main(String[] args) {
 		
+		//아이유
 		Song s1 = new Song();
-		s1.settitle("좋은날");
-		s1.setartist("아이유");
-		//s1.setalbum('REAL');
-		s1.setyear('2010');
-		s1.settrack("3번 track");
-		s1.setcomposer("이민수 작곡");
+		s1.setTitle("좋은날");
+		s1.setArtist("아이유");
+		s1.setAlbum("Real");
+		s1.setYear("2010");
+		s1.setTrack("3번 track");
+		s1.setComposer("이민수 작곡");
 		
 		Song s2 = new Song();
-		s2.settitle("거짓말");
-		s2.setartist("BIGBANG");
-		//s2.setalbum('Always');
-		s2.setyear('2007');
-		s2.settrack("2번 track");
-		s2.setcomposer("G-DRAGON 작곡");
+		s2.setTitle("거짓말");
+		s2.setArtist("BIGBANG");
+		s2.setAlbum("Always");
+		s2.setYear("2007");
+		s2.setTrack("2번 track");
+		s2.setComposer("G-DRAGON 작곡");
 		
-		/*
 		Song s3 = new Song();
-		s3.settitle("벚꽃엔딩");
-		s3.setartist("버스커버스커");
-		s3.setalbum("버스커버스커1집");
-		s3.setyear('2012');
-		s3.settrack('4번 track');
-		s3.setcomposer('장범준 작곡');
-		*/
+		s3.setTitle("벚꽃엔딩");
+		s3.setArtist("버스커버스커");
+		s3.setAlbum("버스커버스커1집");
+		s3.setYear("2012");
+		s3.setTrack("4번 track");
+		s3.setComposer("장범준 작곡");
+		
 	}
 }
 
