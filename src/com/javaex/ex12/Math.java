39d33e3cafb23a6ef924615bac3c1d16ec716acd
ahah2025 <adghty61>
2025-04-30 17:00:04 +0900

    2025-04-30

---
+++ src/com/javaex/ex12/Math.java
@@ -0,0 +1,57 @@
+package com.javaex.ex12;
+
+public class Math {
+
+	//메소드 오버로딩 예제
+	
+	//필드
+	
+	//생성자
+	//기본생성자 자동으로 생김
+
+	//메소드 gs
+	
+	//메소드 일반
+	/*
+	public int plusIntInt(int a, int b) {
+		return a+b;
+	}
+	 	
+	public double plusIntDouble(int a, double b) {
+		return a+b;
+	}
+	 	
+	public double plusDoubleInt(double a, int b) {
+		return a+b;
+	}
+	 	
+	public double plusDoubleDouble(double a, double b) {
+		return a+b;
+	}
+	*/
+	
+	public int plus(int a, int b) {
+ 		return a+b;
+ 	}
+ 	
+ 	public double plus(int a, double b) {
+ 		return a+b;
+ 	}
+ 	
+ 	public double plus(double a, int b) {
+ 		return a+b;
+ 	}
+	
+	public double plus(double a, double b) {
+ 		return a+b;	
+ 	}
+ 	
+ 	public double plus(int a, int b, int c) {
+ 		return a+b+c;	
+ 	}
+	
+	
+	
+	
+	
+}
