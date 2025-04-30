f8f7cc1b78246074c8e212420b7278c15adecfaa
ahah2025 <adghty61>
2025-04-30 10:22:54 +0900

    2025-04-29

--- src/com/javaex/ex07/song/SongApp.java
+++ src/com/javaex/ex07/song/SongApp.java
@@ -11,7 +11,7 @@ public class SongApp {
 		S1.setAlbum("Real");
 		S1.setYear("2010");
 		S1.setTrack("3번 track");
-		S1.setComposer("이민수 작곡");
+		S1.setComposer(" 이민수 작곡");
 		
 		//BIGBANG
 		Song S2 = new Song();
@@ -20,19 +20,19 @@ public class SongApp {
 		S2.setAlbum("Always");
 		S2.setYear("2007");
 		S2.setTrack("2번 track");
-		S2.setComposer("G-DRAGON 작곡");
+		S2.setComposer(" G-DRAGON 작곡");
 		
 		//버스커버스커
 		Song S3 = new Song();
 		S3.setTitle(" 벚꽃엔딩");
 		S3.setArtist("버스커버스커");
 		S3.setAlbum("버스커버스커1집");
-		S3.setYear("2012");
+		S3.setYear(" 2012");
 		S3.setTrack("4번 track");
-		S3.setComposer("장범준 작곡");
+		S3.setComposer(" 장범준 작곡");
 		
-		S1.showInfo();
 		S2.showInfo();
+		S1.showInfo();
 		S3.showInfo();
 	}
 }
