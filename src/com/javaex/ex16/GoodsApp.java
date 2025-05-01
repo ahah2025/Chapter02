7ea8168e852021aadec36713e51e0452907d8ddd
ahah2025 <adghty61>
2025-05-01 15:05:16 +0900

    2025-05-01

---
+++ src/com/javaex/ex16/GoodsApp.java
@@ -0,0 +1,28 @@
+package com.javaex.ex16;
+
+import com.javaex.ex14.Goods;
+
+public class GoodsApp {
+
+	public static void main(String[] args) {
+		
+		Goods cup = new Goods("머그컵",2000);
+		Goods camera = new Goods("니콘",400000);
+		Goods laptop = new Goods("LG그램",900000);
+		
+		/*
+		System.out.println(cup.toString());
+		System.out.println(camera.toString());
+		System.out.println(laptop.toString());
+		*/
+		
+		cup.showInfo();
+		//camera.showInpo();
+		//laptop.showInpo();
+		
+		System.out.println("-------------------------");
+		System.out.println(cup.getName());
+		System.out.println(camera.getName());
+		System.out.println(laptop.getName());
+	}
+}
