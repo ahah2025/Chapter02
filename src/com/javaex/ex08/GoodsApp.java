b0e29d19af130c46dab6671171b255d3699094b9
ahah2025 <adghty61>
2025-05-02 10:19:22 +0900

    2025-05-02

--- src/com/javaex/ex08/GoodsApp.java
+++ src/com/javaex/ex08/GoodsApp.java
@@ -3,7 +3,7 @@ package com.javaex.ex08;
 public class GoodsApp {
 
 	public static void main(String[] args) {
-		
+			 //7번 Line camera는 우리가 정한 변수	
 		Goods camera = new Goods();
 		camera.setName("니콘");
 		camera.setPrice(400000);
