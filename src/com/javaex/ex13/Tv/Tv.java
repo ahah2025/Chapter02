1833193158961ada9a9f688663bdf4ce1320171b
ahah2025 <adghty61>
2025-05-02 11:19:35 +0900

    ex13.tv

--- src/com/javaex/ex13/Tv/Tv.java
+++ src/com/javaex/ex13/Tv/Tv.java
@@ -3,60 +3,112 @@ package com.javaex.ex13.Tv;
 public class Tv {
 
 	//필드
-	private int volume;
-	private boolean volume01;
-	private int power;
 	private int channel;
+	private int volume;
+	private boolean power;
 	
 	//생성자
 	public Tv() {
 		//메모리
-		System.out.println("Volume(100)");
 	}
 	
-	public Tv(int volume,int power,int channel) {
+	public Tv(int channel,int volume,boolean power) {
 		//메모리에 올려주는 일
+		this.channel = channel;
 		this.volume = volume;
 		this.power = power;
-		this.channel = channel;
-		System.out.println("Channel(7)");
 	}
 	
 	//메소드 gs
 	
 	//전원(power) 대입
-	public void setPower(int power) {
-		this.power = power;
-	} 	 	
-	//전원(power) 읽기
-	public int getPower() {
-		return power;
-	}	
-	
+	public void setPower(boolean on) {
+		if(on ==true) {
+			power = true;
+		}else {
+			power =false;
+		}
+	} 	
 	
 	//볼륨 대입
 	public void setVolume(int volume) {
-		this.volume = volume;
+		if(power == true) { //파워가 true이면 작동
+			if(volume < 0) {
+				this.volume = 0;		
+			
+			}else if(volume > 100) {
+				this.volume = 100;
+			
+			}else {
+				this.volume = volume;
+			}
+			
+		}else { //파워가 off이면 작동X
+			System.out.println("전원이 꺼져있습니다.");
+		}
 	}
-	//볼륨읽기
-	public int getVolume() {
-		return volume;
+
+	//볼륨 대입
+	public void setVolume(boolean up) {
+		if(power == true) { //파워가 true이면 작동
+			//볼륨 올리기/내리기
+			if(up == true ) {
+				volume = volume + 1; //up이 true이면 채널 +1	
+			}else {
+				volume = volume - 1; //up이 false이면 채널 -1
+			}
+			
+			//볼륨 최대/최소값 보정
+			if(volume < 0) { //현재볼륨이 0보다 작으면 0로
+				this.volume = 0;		
+			
+			}else if(volume > 100) { //현재채널이 100보다 크면 100로
+				this.volume = 100;
+				
+			}
+			
+		}else { //파워가 off이면 작동X
+			System.out.println("전원이 꺼져있습니다.");
+		}
 	}	
-	//볼륨1 대입
-	public void setVolume01(boolean volume) {
-		this.volume01 = volume01;
-	}
-	//볼륨1 읽기
-	public boolean getVolume01() {
-		return volume01;
-	}
 	//channel 대입
 	public void setChannel(int channel) {
-		this.channel = channel;
+		if(power == true) {  //파워가 true이면 작동
+			if(channel < 1) {
+				this.channel =1;
+			}else if(channel > 255) {
+				this.channel =255;
+			}else {
+				this.channel = channel;
+			}
+		}else { //파워가 off이면 작동 X
+			System.out.println("전원이 꺼져있습니다.");
+		}
 	}
-	//channel 읽기
-	public int getChannel() {
-		return channel;
+	
+	public void setChannel(boolean up) {
+		if(power == true) { //파워가 true이면 작동
+			
+			//채널 올리기/내리기
+			if(up == true ) {
+				channel = channel + 1; //up이 true이면 채널 +1	
+			}else {
+				channel = channel - 1; //up이 false이면 채널 -1
+			}
+			
+			//채널 최대/최소값 보정
+			if(channel < 1) { //현재채널이 1보다 작으면 1로
+				this.channel = 1;		
+			
+			}else if(channel > 255) { //현재채널이 255보다 크면 255로
+				this.channel = 255;
+				
+			}
+			
+		}else { //파워가 off이면 작동X
+			System.out.println("전원이 꺼져있습니다.");
+		}
+
 	}
 	
 	
@@ -64,8 +116,6 @@ public class Tv {
 	
 	//Tv정보 출력
 	public void status() {
-		System.out.print("파워"+power+"\t");
-		System.out.print("채널"+channel+"\t");
-		System.out.print("볼륨"+volume);
+		System.out.println("파워:" + power + " 채널:" + channel + " 볼륨:" + volume);
 	}	
 }
--- src/com/javaex/ex13/Tv/TvApp.java
+++ src/com/javaex/ex13/Tv/TvApp.java
@@ -3,19 +3,83 @@ package com.javaex.ex13.Tv;
 public class TvApp {
 
 	public static void main(String[]args) {
-		Tv tv = new Tv();
+		Tv tv = new Tv(7,20,false);
 		
-		tv.setVolume(100); //100으로 유지
-		
-		tv.setVolume(15);
-		
-		tv.setChannel(11);
+		//전원 off테스트
+		tv.setChannel(true); 
+		tv.setVolume(45);
+		tv.status();
+		System.out.println("------------------------");
 			
-		tv.getPower();
+		tv.setPower(true);
+		tv.status();
+		System.out.println("------------------------");
+				
+		//채널 최고값 테스트
+		tv.setChannel(300); 
+		tv.status();
+				
+		tv.setChannel(254); 
+		tv.status();
+				
+		tv.setChannel(true);
+		tv.status();
+				
+		tv.setChannel(true);
+		tv.status();
+		System.out.println("------------------------");
+				
+		//채널 최저값 테스트
+		tv.setChannel(0); 
+		tv.status();
+				
+		tv.setChannel(2); 
+		tv.status();
+				
+		tv.setChannel(false);
+		tv.status();
+				
+		tv.setChannel(false);
+		tv.setChannel(false);
+		tv.status();
+				
+		System.out.println("------------------------");
+			
+		//볼륨 최고값 테스트
+		tv.setVolume(150); 
+		tv.status();
+				
+		tv.setVolume(98); 
+		tv.status();
+				
+		tv.setVolume(true);
+		tv.status();
+				
+		tv.setVolume(true);
+		tv.setVolume(true);
+		tv.setVolume(true);
+		tv.status();
+		System.out.println("------------------------");
+				
+		//볼륨 최저값 테스트
+		tv.setVolume(-20); 
+		tv.status();
+				
+		tv.setVolume(2); 
+		tv.status();
+				
+		tv.setVolume(false);
+		tv.status();
+				
+		tv.setVolume(false);
+		tv.setVolume(false);
+		tv.setVolume(false);
+		tv.status();
+				
+		System.out.println("------------------------");
+				
+		//전원끄기
+		tv.setPower(false);
 		tv.status();
 	}
 }
-/*
-Volume(100)
-파워0		채널11	볼륨15
-*/
