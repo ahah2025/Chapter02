0c0bdfee98b77184150b815483fc5ee4b3df287f
ahah2025 <adghty61>
2025-05-07 14:19:37 +0900

    2025-05-07

--- src/com/javaex/ex22/ShapeApp.java
+++ src/com/javaex/ex22/ShapeApp.java
@@ -7,11 +7,14 @@ public class ShapeApp {
 		Shape s01 = new Shape("빨강","검정");
 		System.out.println(s01.toString());
 		
+		//선, 면의색이 없는 삼각형은 잘못된것임
 		//Triangle t01 = new Triangle(5,10); //선, 면의색이 없는 삼각형은 잘못된것임
 		
 		Triangle t01 = new Triangle("빨강","검정",5,10);
 		//System.out.println(t01.toString());
 		t01.draw();
+		t01.getWidth();
+		t01.getHeight();
 		
 		Ractangle r01 = new Ractangle("주황","초록",10,20);
 		//System.out.println(r01.toString());
@@ -21,18 +24,18 @@ public class ShapeApp {
 		//System.out.println(c01.toString());
 		c01.draw();
 		
-		//모든 도형을 하나의 배열로 관리하고 싶다.
+		Shape st = new Triangle("초록","초록",100,100);
+		st.getFillColor();
+		//st.draw();
+		//st.getWidth();
+		//st.getHeight();
 		
-		Shape[] sArr = new Shape[3];
-		sArr[0] = t01;  //삼각형
-		sArr[1] = r01;	//사각형
-		sArr[2] = c01;  //원
+		Shape sc = new Circle("빨강","빨강",50);
+		sc.getFillColor();
+		//sc.getRadius();
+		//sc.draw();
 		
-		for(int i=0;i<sArr.length; i++) {
-			System.out.println(sArr[i].getFillColor());
-			System.out.println(sArr[i].getLineColor());
-			System.out.println("--------------------------------");
-		}
+		
 		
 		
 	}
